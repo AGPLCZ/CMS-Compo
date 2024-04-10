294871a384866b126bee5c648d9d9e69e111d910
AGPLCZ <62847503+AGPLCZ@users.noreply.github.com>
2024-04-10 23:12:46 +0200

    new

--- trash/tabulky.sql
+++
@@ -1,9 +0,0 @@
-
--- Přidání sloupce id s auto inkrementem jako primární klíč
-ALTER TABLE `page_components`
-  ADD COLUMN `id` INT AUTO_INCREMENT PRIMARY KEY FIRST;
-
-
--- Přidání sloupce id s auto inkrementem
-ALTER TABLE `page_components`
-  ADD COLUMN `id` INT AUTO_INCREMENT UNIQUE FIRST;
